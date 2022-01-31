8948e88a7e8bfbc6ad4f0dafe9839ac35d180987
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-01-31 16:44:05 -0500

    second commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,17 @@
-import React from "react";
-
-
+import React, { useState } from "react";
 
 function Form(props) {
+  const [name, setName] = useState('');
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
-    props.addTask("Say hello!");
+    props.addTask(name);
+    setName("");
   }
-  
   return (
     <form onSubmit={handleSubmit}>
       <h2 className="label-wrapper">
@@ -22,6 +25,8 @@ function Form(props) {
         className="input input__lg"
         name="text"
         autoComplete="off"
+        value={name}
+        onChange={handleChange}
       />
       <button type="submit" className="btn btn__primary btn__lg">
         Add
