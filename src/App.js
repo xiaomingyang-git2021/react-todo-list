8f40eeda74d41e39d57d0919959cd799004ddcfd
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-01-31 21:39:02 -0500

    ninth commit

--- src/App.js
+++ src/App.js
@@ -27,6 +27,19 @@ function App(props) {
     setTasks(remainingTasks);
   }
 
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        //
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
   const taskList = tasks.map(task => (
     <Todo 
     id={task.id} 
@@ -35,6 +48,7 @@ function App(props) {
     key={task.id} 
     toggleTaskCompleted={toggleTaskCompleted}
     deleteTask={deleteTask}
+    editTask={editTask}
   />
 ));
 
