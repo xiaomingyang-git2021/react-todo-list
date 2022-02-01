aaafd61e4d2e35f233fb87f58cb6dead350f50bc
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-01-31 20:26:00 -0500

    sixth commit

--- src/App.js
+++ src/App.js
@@ -7,12 +7,27 @@ import { nanoid } from "nanoid";
 
 function App(props) {
   const [ tasks, setTasks ] = useState(props.tasks);
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
   const taskList = tasks.map(task => (
     <Todo 
     id={task.id} 
     name={task.name} 
     completed={task.completed}
     key={task.id} 
+    toggleTaskCompleted={toggleTaskCompleted}
   />));
 
 
