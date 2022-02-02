c53f93b4635b0151a5ad972f7408a476985f4472
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-02-02 14:53:42 -0500

    targeting our elements

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 export default function Todo(props) {
   const [ isEditing, setEditing ] = useState(false);
   const [ newName, setNewName ] = useState('');
+  const editFieldRef = useRef(null);
+  const editButtonRef = useRef(null);
 
   function handleChange(e) {
     setNewName(e.target.value);
@@ -27,6 +29,7 @@ export default function Todo(props) {
         type="text"
         value={newName}
         onChange={handleChange}
+        ref={editFieldRef}
       />
       </div>
       <div className="btn-group">
@@ -59,7 +62,12 @@ export default function Todo(props) {
           </label>
         </div>
         <div className="btn-group">
-          <button type="button" className="btn" onClick={() => setEditing(true)}>
+          <button 
+            type="button" 
+            className="btn" 
+            onClick={() => setEditing(true)}
+            ref={editButtonRef}
+          >
             Edit <span className="visually-hidden">{props.name}</span>
           </button>
           <button
